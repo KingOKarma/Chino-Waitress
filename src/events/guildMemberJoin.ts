49003c74227da181773805b7ade94b841528c5d4
KingOKarma <virtualdisaster782@gmail.com>
2021-12-26 04:00:26 +0000

    Added Clonexbot banning functionalilty

--- src/events/guildMemberJoin.ts
+++ src/events/guildMemberJoin.ts
@@ -5,6 +5,11 @@ import { GuildMember } from "discord.js";
 export const event: Event = {
     name: "guildMemberAdd",
     run: async (client, member: GuildMember) => {
-        
+        const { username } = member.user;
+        const { guild } = member;
+        if (username.includes("Clonex")) {
+            await member.send({ "content": "You have been banned from Chinos for being a self-bot, please appeal here if this was a mistake:\n https://forms.gle/PmbrWsURvzcYmfgD6" });
+            await guild.bans.create(member, { "reason": "Clonex Spamming Bot" });
+        }
     }
 };
