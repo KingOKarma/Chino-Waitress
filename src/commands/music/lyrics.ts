987537bdbf9339ef230d8159bd30e86c5d539589
Caden-kun <cadenripepi012@gmail.com>
2022-01-22 20:34:31 +0200

    improved lyric command

--- src/commands/music/lyrics.ts
+++ src/commands/music/lyrics.ts
@@ -14,7 +14,7 @@ export const command: Command = {
     name: "lyrics",
     permissionsBot: rolePerms,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    run: async ({ client, msg }) => {
+    run: async ({ client, msg, args }) => {
 
         const queue = client.queue.get(msg.guild?.id ?? "");
         if (!queue) return client.embedReply(msg, { embed: { description: "There is nothing playing." } } ).catch(console.error);
@@ -26,7 +26,10 @@ export const command: Command = {
         try {
             lyrics = await lyricsFinder("", queue.songs[0].title);
             // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-            if (!lyrics) lyrics = `No lyrics found for ${title}.`;
+            if (!lyrics) lyrics = args;
+            lyrics = await lyricsFinder("", args);
+            // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+            if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}`;
         } catch (error) {
             lyrics = `No lyrics found for ${title}.`;
         }
