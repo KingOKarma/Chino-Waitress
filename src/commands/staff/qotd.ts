965f36ac8774526edbfbff4c881655409a325283
KingOKarma <virtualdisaster782@gmail.com>
2022-05-31 23:47:31 +0100

    grammar 2

--- src/commands/staff/qotd.ts
+++ src/commands/staff/qotd.ts
@@ -20,11 +20,12 @@ export const command: Command = {
 
             if (role.mentionable) {
                 await role.setMentionable(false, "Staff set mentionable");
-                return await client.embedReply(msg, { embed: { description: `The \`${role.name}\` is **no longer** mentionable` } }).catch(console.error);
+                return await client.embedReply(msg, { embed: { description: `The \`${role.name}\` role is **no longer** mentionable` } }).catch(console.error);
             }
+
             await role.setMentionable(true, "Staff set mentionable");
 
-            return await client.embedReply(msg, { embed: { description: `The \`${role.name}\` is **now** mentionable` } }).catch(console.error);
+            return await client.embedReply(msg, { embed: { description: `The \`${role.name}\` role is **now** mentionable` } }).catch(console.error);
 
 
         } catch (err) {
