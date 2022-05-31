c3bfa33b1878fa1601ac9c9d1747c46fe87bd83d
KingOKarma <virtualdisaster782@gmail.com>
2022-05-31 23:22:58 +0100

    Added qotd rotd commmands

---
+++ src/commands/staff/qotd.ts
@@ -0,0 +1,36 @@
+import { Command } from "../../interfaces";
+import { rolePerms } from "../../globals";
+
+export const command: Command = {
+    cooldown: 3,
+    description: "Change the mentionability of the qotd role",
+    example: ["!uptime"],
+    group: "staff",
+    staffOnly: true,
+    guildOnly: true,
+    name: "qotd",
+    permissionsBot: rolePerms,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    run: async ({ client, msg }) => {
+
+        try {
+            const role = await msg.guild?.roles.fetch("659832384874741800");
+
+            if (!role) return Error();
+
+            if (role.mentionable) {
+                await role.setMentionable(false, "Staff set mentionable");
+                return await client.embedReply(msg, { embed: { description: `The \`${role.name}\` is **no longer** mentionable` } }).catch(console.error);
+            }
+            await role.setMentionable(true, "Staff set mentionable");
+
+            return await client.embedReply(msg, { embed: { description: `The \`${role.name}\` is **now** mentionable` } }).catch(console.error);
+
+
+        } catch (err) {
+            return await client.embedReply(msg, { embed: { description: "There was an error finding the qotd role, does it still exist?" } }).catch(console.error);
+
+        }
+
+    }
+};
---
+++ src/commands/staff/rotd.ts
@@ -0,0 +1,37 @@
+import { Command } from "../../interfaces";
+import { rolePerms } from "../../globals";
+
+export const command: Command = {
+    cooldown: 3,
+    description: "Change the mentionability of the rotd role",
+    example: ["!uptime"],
+    group: "staff",
+    staffOnly: true,
+    guildOnly: true,
+    name: "rotd",
+    permissionsBot: rolePerms,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    run: async ({ client, msg }) => {
+
+        try {
+            // I dont have the effort rn to be asked to do all the db stuff, so heres a hardcode
+            const role = await msg.guild?.roles.fetch("659832485344968725");
+
+            if (!role) return Error();
+
+            if (role.mentionable) {
+                await role.setMentionable(false, "Staff set mentionable");
+                return await client.embedReply(msg, { embed: { description: `The \`${role.name}\` is **no longer** mentionable` } }).catch(console.error);
+            }
+            await role.setMentionable(true, "Staff set mentionable");
+
+            return await client.embedReply(msg, { embed: { description: `The \`${role.name}\` is **now** mentionable` } }).catch(console.error);
+
+
+        } catch (err) {
+            return await client.embedReply(msg, { embed: { description: "There was an error finding the rotd role, does it still exist?" } }).catch(console.error);
+
+        }
+
+    }
+};
